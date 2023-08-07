0e822622791774adb809b5e390d9df4b4bb0cf56
YoungEunYu <jgjrjej@ewhain.net>
2023-08-07 17:22:24 +0900

    rebase

--- web/src/config.py
+++
@@ -1,5 +0,0 @@
-CLIENT_ID = "7ba50bb3cf2136baafa3e8a344926bc3"
-CLIENT_SECRET = "0bbVT54CBT7J66HkbjZ4m1QiGONyocz8"
-REDIRECT_URI = "http://127.0.0.1:5000/oauth" # 로그인 이후에 갈 URL
-
-SIGNOUT_REDIRECT_URI = "http://127.0.0.1:5000/logout"
